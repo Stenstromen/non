acce3aaee0938bcb86697acee1917b3596ce58c6
Stenstromen <kontakt@filipstenstrom.se>
2024-05-20 01:01:10 +0200

    removed comments and adjusted workflow

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
-use std::io::{self, Read};
+use std::io::{ self, Read };
 
 fn main() {
-    // Define the readme message
-    let readme = "Usage: non [-r | -w | -h]
+    let readme: &str =
+        "Usage: non [-r | -w | -h]
     
 This program reads input from stdin, concatenates lines by removing newline characters (default), and prints the unwrapped output to stdout.
 
@@ -13,30 +13,28 @@ Options:
     -h, --help             Show this help message.
 
 Examples:
-    cat input.txt | ./non
-    cat input.txt | ./non -r
-    cat input.txt | ./non -w
+    cat input.txt | non
+    cat input.txt | non -r
+    cat input.txt | non -w
 ";
 
-    // Check for command line arguments
     let args: Vec<String> = env::args().collect();
-    let handle_cr = args.contains(&"-r".to_string()) || args.contains(&"--carriage-return".to_string());
-    let handle_windows = args.contains(&"-w".to_string()) || args.contains(&"--windows".to_string());
-    let show_help = args.contains(&"-h".to_string()) || args.contains(&"--help".to_string());
+    let handle_cr: bool =
+        args.contains(&"-r".to_string()) || args.contains(&"--carriage-return".to_string());
+    let handle_windows: bool =
+        args.contains(&"-w".to_string()) || args.contains(&"--windows".to_string());
+    let show_help: bool = args.contains(&"-h".to_string()) || args.contains(&"--help".to_string());
 
     if show_help {
         println!("{}", readme);
         return;
     }
 
-    // Create a new String to hold the input
-    let mut input = String::new();
+    let mut input: String = String::new();
 
-    // Read the entire input from stdin
     match io::stdin().read_to_string(&mut input) {
         Ok(_) => {
-            // Remove the specified characters
-            let unwrapped = if handle_cr {
+            let unwrapped: String = if handle_cr {
                 input.replace("\r", "")
             } else if handle_windows {
                 input.replace("\r\n", "")
@@ -44,9 +42,8 @@ Examples:
                 input.replace("\n", "")
             };
 
-            // Print the unwrapped output without adding a newline
             print!("{}", unwrapped);
-        },
+        }
         Err(err) => {
             eprintln!("Failed to read from stdin: {}", err);
             println!("{}", readme);
